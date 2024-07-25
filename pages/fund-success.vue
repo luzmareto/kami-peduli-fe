c00822f6dfe60deb6d6dcb8fb9cbabb9ad1446b9
Luzmareto Rusmadian <900179@hq.bni.co.id>
2024-07-26 05:17:22 +0700

    FE 4 7 Konversi Halaman   Success Fund

--- pages/fund-success.vue
+++ pages/fund-success.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="container mx-auto h-screen flex justify-center items-center">
+   <div class="container mx-auto h-screen flex justify-center items-center">
         <div class="w-full lg:w-1/3 px-10 lg:px-0">
           <div class="flex justify-center items-center mx-auto mt-6 mb-8">
-            <img src="/success-illustration.svg" alt="" class="w-full" />
+            <img src="/funded-illustration.svg" alt="" class="w-full" />
           </div>
           <h2 class="font-medium mb-3 text-3xl text-center">
-            Welcome onboard!
+            Yeay! You are super
           </h2>
           <p class="text-center font-light">
-            Your account just registered
+            Your money has ben transferred
             <br />
-            into our system
+            into company's account
           </p>
           <div class="mb-4 mt-6">
             <div class="mb-3">
@@ -18,7 +18,15 @@
                 @click="$router.push({ path: '/' })"
                 class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
               >
-                Start Explore
+                Fund Other Project
+              </button>
+            </div>
+            <div class="mb-4">
+              <button
+                @click="$router.push({ path: '/dashboard' })"
+                class="block w-full bg-transparent border border-gray-500 text-gray-500 hover:bg-green-button hover:border-green-button hover:text-white font-light px-6 py-4 text-lg rounded-full"
+              >
+                My Dashboard
               </button>
             </div>
           </div>
@@ -28,7 +36,7 @@
     
     <script>
     export default {
-        layout: 'success',
+        layout: 'success', 
     }
     </script>
     
