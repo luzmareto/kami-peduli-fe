b9bf33c01e7f12d3c368800b1c7fef4d11a3b933
Luzmareto Rusmadian <900179@hq.bni.co.id>
2024-07-29 03:18:02 +0700

    FE 5 6 Integrasi API   Integrasi Landing Page Script

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    'nuxtjs/auth',
+    '@nuxtjs/auth',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
